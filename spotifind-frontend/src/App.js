0ad7a107f5cc9ccf59cbd010b4a7cc79cf89dc39
Drew Vlasnik <drewvlaz@uchicago.edu>
2022-04-02 15:14:05 -0500

    react boilerplate

---
+++ spotifind-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment> 
+      <div className="container mt-2">
+        <h1>SpotiFind</h1>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
